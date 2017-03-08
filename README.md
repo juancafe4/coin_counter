4c0bac3c2a618c1e4a1ab0c740d62b6fe3a2cbfc
Juan Carlos <juancafe2@gmail.com>
2017-03-08 01:36:53 -0400

    Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Coin Count
+Count coins clicking on the respective coin and gets the total. The user can also clear the total.
+# Build with
+* [Materialize CSS](http://materializecss.com/buttons.html)
--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 let total = 0;
+
+// Increase total depending what coin the user clicked
 function increase(val) {
   const inlineBlock = 'inline-block';
   let display = document.getElementById(`add${val}`).style.display;
   let num = Number(document.getElementById(`add${val}`).innerHTML);
- 
+  
+  // If display is none change to inline-block
   if (!display) {
     document.getElementById(`add${val}`).style.display = inlineBlock;
   }
+  // Add the number of coins
   document.getElementById(`add${val}`).innerHTML = num + 1;
+  
+  // Increase total
   total += val;
 }
 
+// Display total and display clear button
 function calculate() {
   document.getElementById("box").innerHTML = total;
   document.getElementById("clear").style.display = 'inline-block';
 }
 
+// Clear all coins and set total to zero
 function clearAll() {
   // Clear everything little notifications off
   document.getElementById('add25').style.display = '';
